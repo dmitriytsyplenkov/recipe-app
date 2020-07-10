d768a0f3d788cce4a74fc449ba848cc96b9af6e6
DIMITREE <dmitriytsyplenkov@gmail.com>
2020-07-10 09:58:57 +0300

    changed some stuff inside index controller

--- src/main/java/com/dts/recipeapp/controllers/IndexController.java
+++ src/main/java/com/dts/recipeapp/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"","/","/index"})
     public String getIndexPage() {
-        System.out.println("Some message to say ... 1234");
+        System.out.println("Some message to say ... hello man");
         return "index";
     }
 
