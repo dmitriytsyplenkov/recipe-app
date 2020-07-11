5e1d3acfd0b35cf5542c644940578447cc217b68
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-07-11 15:02:07 +0300

    Added Mokito Unit tests on RecipeServiceImpl

---
+++ src/test/java/com/dts/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.dts.recipeapp.services;
+
+import com.dts.recipeapp.domain.Recipe;
+import com.dts.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+@DisplayName("RecipeServiceImpl unit tests")
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    @DisplayName("Testing getRecipes method")
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(),1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
