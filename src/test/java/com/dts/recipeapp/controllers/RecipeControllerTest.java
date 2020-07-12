d1b7c0bc56bdfd8b90ae5f16624be42acdb3d786
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-07-12 22:58:11 +0300

    Added Recipe information by ID on different page

---
+++ src/test/java/com/dts/recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,47 @@
+package com.dts.recipeapp.controllers;
+
+import com.dts.recipeapp.domain.Recipe;
+import com.dts.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeControllerTest {
+    @Mock
+    RecipeService recipeService;
+    @InjectMocks
+    RecipeController controller;
+
+    @BeforeEach
+    public void setUp()  {
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+
+}
--- src/test/java/com/dts/recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/dts/recipeapp/services/RecipeServiceImplTest.java
@@ -9,10 +9,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+
 @DisplayName("RecipeServiceImpl unit tests")
 class RecipeServiceImplTest {
 
@@ -26,7 +29,20 @@ class RecipeServiceImplTest {
 
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
+    @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
 
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
     @Test
     @DisplayName("Testing getRecipes method")
     void getRecipes() {
