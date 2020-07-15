15e8fd1564b5e279e143a35e6ff811fa528eeb37
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-07-15 22:07:51 +0300

    Added Uploading Images

---
+++ src/main/java/com/dts/recipeapp/services/ImageService.java
@@ -0,0 +1,7 @@
+package com.dts.recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long valueOf, MultipartFile file);
+}
---
+++ src/test/java/com/dts/recipeapp/controllers/ImageControllerTest.java
@@ -0,0 +1,69 @@
+package com.dts.recipeapp.controllers;
+
+import com.dts.recipeapp.commands.RecipeCommand;
+import com.dts.recipeapp.services.ImageService;
+import com.dts.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class ImageControllerTest {
+
+    @Mock
+    ImageService imageService;
+
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    ImageController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void getImageForm() throws Exception {
+        //given
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService).findCommandById(anyLong());
+    }
+
+
+    @Test
+    public void testHandleImagePost() throws Exception {
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("imagefile","testing.txt","text/plain",
+                "Some text to convert".getBytes());
+        mockMvc.perform(multipart("/recipe/1/image").file(mockMultipartFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location","/recipe/1/show"));
+        verify(imageService).saveImageFile(anyLong(),any());
+    }
+}
