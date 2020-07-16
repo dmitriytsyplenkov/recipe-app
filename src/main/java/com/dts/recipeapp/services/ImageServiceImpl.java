2aad473b9dc4f546962e20190c4c9a3942b5f0c5
dimitree <dmitriytsyplenkov@gmail.com>
2020-07-16 07:52:10 +0300

    added some additional checks to image saving method

--- src/main/java/com/dts/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/dts/recipeapp/services/ImageServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -21,24 +22,28 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        Recipe recipe = recipeRepository.findById(recipeId).get();
-        try {
-            Byte[] byteObjects = new Byte[file.getBytes().length];
-            int i = 0;
-            for (byte b :
-                    file.getBytes()) {
-                byteObjects[i++] = b;
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if (recipeOptional.isPresent()) {
+            Recipe recipe = recipeOptional.get();
+            try {
+                Byte[] byteObjects = new Byte[file.getBytes().length];
+                int i = 0;
+                for (byte b :
+                        file.getBytes()) {
+                    byteObjects[i++] = b;
+                }
+                recipe.setImage(byteObjects);
+
+                recipeRepository.save(recipe);
+
+            } catch (IOException exception) {
+                //todo handle it later
+                log.error("Error occurred");
+                exception.printStackTrace();
             }
-            recipe.setImage(byteObjects);
-
-            recipeRepository.save(recipe);
-
-        } catch (IOException exception) {
-            //todo handle it later
-            log.error("Error occurred");
-            exception.printStackTrace();
+        } else {
+            log.error("Attempt to upload image for not existing recipe id: " + recipeId);
         }
-
-
     }
 }
