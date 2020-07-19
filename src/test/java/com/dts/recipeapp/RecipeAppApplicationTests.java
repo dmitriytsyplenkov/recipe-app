34bcb88443e27c40f796bc1dc03164ff7a738c9d
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-07-19 16:17:33 +0300

    Fixing circle-ci problems

--- src/test/java/com/dts/recipeapp/RecipeAppApplicationTests.java
+++ src/test/java/com/dts/recipeapp/RecipeAppApplicationTests.java
@@ -1,12 +1,16 @@
 package com.dts.recipeapp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-default.properties")
 class RecipeAppApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
