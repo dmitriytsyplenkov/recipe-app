d840bef68793d1960a1ab557c345c21e5bf00c27
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-07-19 16:27:58 +0300

    Fixing circle-ci problems agane

--- src/test/java/com/dts/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/dts/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
+@ActiveProfiles("default")
 class UnitOfMeasureRepositoryIT {
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
